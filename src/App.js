40378467615abac996eed24fefc26a5185233e6d
Dimitri <d.habruk@gmail.com>
2023-01-06 00:29:22 +0100

    button not work

--- src/App.js
+++ src/App.js
@@ -8,16 +8,33 @@ function App() {
 const[contacts5, setContacts5] =useState(contacts.slice(0, 5))
 // console.log(contacts5)
 
+const[createEl, setCreateEl] =useState([
+
+  <tbody>
+
+                  {contacts5.map(elem => 
+                    <tr key={elem.id}>
+                    <td><h2 className='nameActeur'>{elem.name}</h2> </td>
+                    <td><img src={elem.pictureUrl} className='imgActers'/></td>
+                    <td><h3>{elem.popularity}</h3></td>
+                    
+                    
+                    </tr> 
+                  )}
+                 
+       </tbody>]
+       )
+
 
 useEffect(() => {
   return console.log(contacts5)
 
 },[]);
- 
 
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button onClick={()=>setCreateEl([0])}>Add Random Acteur</button>
       
       <table>
 		    <thead>
@@ -31,10 +48,10 @@ useEffect(() => {
         <tbody>
 
                   {contacts5.map(elem => 
-                    <tr>
-                    <td>{elem.name}</td>
+                    <tr key={elem.id}>
+                    <td><h2 className='nameActeur'>{elem.name}</h2> </td>
                     <td><img src={elem.pictureUrl} className='imgActers'/></td>
-                    <td>{elem.popularity}</td>
+                    <td><h3>{elem.popularity}</h3></td>
                     
                     
                     </tr> 
