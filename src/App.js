d17b45d23699e684c578b75780b2997d9f8c1707
Dimitri <d.habruk@gmail.com>
2023-01-05 23:45:59 +0100

    done for 30%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import contacts from "./contacts.json";
 
-function App() {
+function App() { 
+
+// const contacts5 = contacts.slice(0, 5)
+const[contacts5, setContacts5] =useState(contacts.slice(0, 5))
+// console.log(contacts5)
+
+
+useEffect(() => {
+  return console.log(contacts5)
+
+},[]);
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      
+      <table>
+		    <thead>
+			    <tr>
+            <th>Name</th>
+            <th>Picture</th>
+    				<th>Popularity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+                  {contacts5.map(elem => 
+                    <tr>
+                    <td>{elem.name}</td>
+                    <td><img src={elem.pictureUrl} className='imgActers'/></td>
+                    <td>{elem.popularity}</td>
+                    
+                    
+                    </tr> 
+                  )}
+                 
+       </tbody>
+
+    </table>
+
+
+      {/* {contacts.map(elem => 
+      
+
+        <table>
+		<thead>
+			<tr>
+				<th>Picture</th>
+				<th>Name</th>
+				<th>Popularity</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>{elem.name}</td>
+				<td><img src={elem.pictureUrl} className='imgActers'/></td>
+				<td>{elem.popularity}</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+			</tr>
+		</tbody>
+	</table> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
